335c997caf47f8deeb4be40fa036a6f7500dc257
brehan.mohamed <brehan.mohamed@objects.ws>
2022-02-11 12:25:44 +0200

    ch11

--- books/tests.py
+++ books/tests.py
@@ -2,13 +2,16 @@ from turtle import title
 from urllib import response
 from django.test import TestCase
 from django.urls  import reverse
-from .models import Book
+from .models import Book, Review
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
 
 class BookTest(TestCase):
     def setUp(self) -> None:
         self.book =Book.objects.create(title='Harry Pooter',author='Jk Rowling',price='24')
+        self.user = get_user_model().objects.create_user(username='reviewuser',email='reviewuser@email.com',password='testpass123')
+        self.review = Review.objects.create(book=self.book,author=self.user,review='An excellent review')
     def test_book_listing(self):
         self.assertEqual(f'{self.book.title}','Harry Pooter')
         self.assertEqual(f'{self.book.author}','Jk Rowling')
@@ -24,5 +27,6 @@ class BookTest(TestCase):
         self.assertEqual(response.status_code,200)
         self.assertEqual(no_response.status_code,404)
         self.assertContains(response,'Harry Pooter')
+        self.assertContains(response,'An excellent review')
         self.assertTemplateUsed(response,'books/book_detail.html')
 
