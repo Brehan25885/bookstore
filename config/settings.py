3993eb538f5f56ed9e619239bb8b1cb2e68d9b74
brehan.mohamed <brehan.mohamed@objects.ws>
2022-02-14 22:14:55 +0200

    ch16

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from email.policy import default
 from pathlib import Path
 from environs import Env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,11 +25,15 @@ env.read_env()
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG")
+DEBUG = env.bool("DJANGO_DEBUG",default=False)
 
 ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
-
-
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT",default=True)
+SECURE_HSTS_SECONDS = env.int("DJANGO_SECURE_HSTS_SECONDS",default=2592000)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool('DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS',default=True)
+SECURE_HSTS_PRELOAD = env.bool('DJANGO_SECURE_HSTS_PRELOAD',default=True)
+SESSION_COOKIE_SECURE = env.bool('DJANGO_SESSION_COOKIE_SECURE',default=True)
+CSRF_COOKIE_SECURE = env.bool('DJANGO_CSRF_COOKIE_SECURE',default=True)
 # Application definition
 
 INSTALLED_APPS = [
